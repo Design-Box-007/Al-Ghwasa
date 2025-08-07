dad8eafe8d53ee6a5e51ce56e72161ac39d1f35b
Himayun <himayun13@gmail.com>
2025-08-07 21:15:06 +0530

    fix: resolve changes before switching to hide-frying-oil-and-mx3

--- src/data/comman.ts
+++ src/data/comman.ts
@@ -20,12 +20,8 @@ export const navLinks: NavLinksType[] = [
       //   navHref: "/mx-3"
       // },
       { navTitle: "Gastec", navHref: "/gastec" },
-<<<<<<< HEAD
-      // { navTitle: "Alla France", navHref: "/frying-oil-tester" },
-=======
       { navTitle: "Gastec-2", navHref: "/gastec-2" },
-      { navTitle: "Alla France", navHref: "/frying-oil-tester" },
->>>>>>> 72bd7b2 (feat: Completed Gastec page UI as per Figma)
+      // { navTitle: "Alla France", navHref: "/frying-oil-tester" },
       // { navTitle: "Frying Oil Tester", navHref: "/products/frying-oil-tester" },
       // { navTitle: "Products", navHref: "/our-product" },
     ],

e982335e7c911a22543e1370ca4aa42e0515e9ed
Akram-00 <aliakram9789@gmail.com>
2025-10-08 15:57:32 +0530

    Add GTM and noscript snippet for analytics tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Google Analytics Scripts */}
+        {/* ✅ Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-N6HT69WK');
+          `}
+        </Script>
+
+        {/* ✅ Google Analytics */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-6TWP40MEWL"
           strategy="afterInteractive"
@@ -35,7 +46,18 @@ export default function RootLayout({
           `}
         </Script>
       </head>
+
       <body>
+        {/* ✅ Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N6HT69WK"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <ToastContainer />
         <Navbar />
         <GoogleAnalytics /> {/* 👈 track route changes */}
